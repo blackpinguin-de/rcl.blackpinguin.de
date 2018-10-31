c2b9d392dd854e072bd1c3996aa98ec068d975a5
Robin C. Ladiges <rcl@blackpinguin.de>
2018-10-31 05:08:32 +0100

    private books

--- literature.php
+++ literature.php
@@ -3,8 +3,10 @@
 <?php include("include1.php"); ?>
 
 <style type="text/css" scoped="scoped">
-table#literature > tbody > tr > td:nth-of-type(4) { background-color: #0f0; color: black; text-align: center; padding: 2px; }
-table#literature > tbody > tr > td:nth-of-type(5) { color: black; text-align: center; padding: 2px; }
+table#literature  td:not(:last-of-type) { padding-right: 5px; }
+table#literature2 td:not(:last-of-type) { padding-right: 5px; }
+table#literature > tbody > tr > td:nth-of-type(5) { background-color: #0f0; color: black; text-align: center; padding: 2px; }
+table#literature > tbody > tr > td:nth-of-type(6) { color: black; text-align: center; padding: 2px; }
 table#literature > tbody > tr > td.c200 { background-color: #00cc00; }
 table#literature > tbody > tr > td.c170 { background-color: #00db00; }
 table#literature > tbody > tr > td.c100 { background-color: #00ff00; }
@@ -22,10 +24,10 @@ table#literature > tbody > tr > td.c0   { background-color: #ff0000; }
 
 <div>
 <?php if($rcl->lang === "de") { ?>
-Diese Seite listet Fachbücher auf die ich gelesen habe,
+Diese Liste enthält Fachbücher die ich gelesen habe,
 und bietet eine Selbsteinschätzung von mir darüber wieviel ich meine vom Inhalt verstanden zu haben.
 <?php } else { ?>
-This site lists text books that I have read.
+This list contains text books that I have read.
 It also shows an self-evaluation of how much I think I have grasped of their content.
 <?php }?>
 </div>
@@ -34,9 +36,9 @@ It also shows an self-evaluation of how much I think I have grasped of their con
 <table id="literature">
 <thead>
     <?php if($rcl->lang === "de") { ?>
-    <tr>  <th>Spr.</th>  <th>Titel</th>  <th>Autor(en)</th>  <th>Gelesen</th>  <th>Verständnis</th>  </tr>
+    <tr>  <th>#</th>  <th>Spr.</th>  <th>Titel</th>  <th>Autor(en)</th>  <th>Gelesen</th>  <th>Verständnis</th>  </tr>
     <?php } else { ?>
-    <tr>  <th>Lng.</th>  <th>Title</th>  <th>Author(s)</th>  <th>Read</th>  <th>Grasp</th>  </tr>
+    <tr>  <th>#</th>  <th>Lng.</th>  <th>Title</th>  <th>Author(s)</th>  <th>Read</th>  <th>Grasp</th>  </tr>
     <?php } ?>
 </thead>
 <tbody>
@@ -157,21 +159,6 @@ It also shows an self-evaluation of how much I think I have grasped of their con
 		'desc' => [ "ISBN 978-3-86490-133-1", "", "Subject: computer forensics" ],
 		'prog' => [ "100%", "85%" ],
 		],
-		[
-		'book' => [ "DE", "Grenzen des Wachstums - das 30-Jahre-Update", "Donella Meadows, Jørgen Randers, Dennis Meadows" ],
-		'desc' => [ "ISBN 978-3-7776-2228-6", "", "English: Limits to Growth: The 30-Year Update" ],
-		'prog' => [ "200%", null ],
-		],
-		[
-		'book' => [ "EN", "Delusions of Gender", "Cordelia Fine" ],
-		'desc' => [ "ISBN 978-184831-220-3", "Thema: Geschlechterforschung", "Subject: Gender studies" ],
-		'prog' => [ "100%", null ],
-		],
-		[
-		'book' => [ "EN", "The God Delusion", "Richard Dawkins" ],
-		'desc' => [ "ISBN 978-0-618-91824-9", "Thema: Religionskritik", "Subject: Criticism of religion" ],
-		'prog' => [ "200%", null ],
-		],
 	];
 	
 	$color = function ($prog) {
@@ -205,8 +192,12 @@ It also shows an self-evaluation of how much I think I have grasped of their con
 		}
 	};
 	
+	$nr = 1;
 	foreach ($books as $b) {
 		echo "\n<tr>";
+		
+		// Nr.
+		echo "  <td>".($nr++)."</td>";
 		
 		// language
 		echo "  <td>".$b['book'][0]."</td>";
@@ -228,5 +219,635 @@ It also shows an self-evaluation of how much I think I have grasped of their con
 ?>
 </tbody>
 </table>
+
+<br/>
+
+<div>
+<?php if($rcl->lang === "de") { ?>
+Folgend weitere Bücher die ich privat gelesen habe, welche jedoch nicht von Informatik handeln.
+<?php } else { ?>
+Additional books that I read privately, which aren't text books for computer science.
+<?php }?>
+</div>
+<br/>
+
+<table id="literature2">
+<thead>
+    <?php if($rcl->lang === "de") { ?>
+    <tr>  <th>#</th>  <th>Spr.</th>  <th>Titel</th>  <th>Autor(en)</th>  <th>Genre</th>  </tr>
+    <?php } else { ?>
+    <tr>  <th>#</th>  <th>Lng.</th>  <th>Title</th>  <th>Author(s)</th>  <th>Genre</th>  </tr>
+    <?php } ?>
+</thead>
+<tbody>
+<?php
+	$fantasy  = "Fantasy";
+	$thriller = "Thriller";
+	$scifi    = [ "Science-Fiction", "Science Fiction" ];
+	$comic    = "Comic";
+	$romance  = [ "Liebesroman", "Romance" ];
+	$novel    = [ "Roman", "Novel" ];
+	
+	$signed = call_user_func(function () {
+		$out = null;
+		return function () use (&$out) {
+			global $rcl;
+			return $out ?? $out = $rcl->newFootStr($rcl->lang("Mein Exemplar wurde vom Author signiert.", "My copy is signed by the author."));
+		};
+	});
+	
+	### +++ Science
+	$science = [
+		[
+		'book' => [ "EN", "The Origin of Species", "Charles Darwin" ],
+		'genr' => "Evolution",
+		],
+		[
+		'book' => [ "EN", "The God Delusion", "Richard Dawkins" ],
+		'genr' => "Religion",
+		'desc' => [ "ISBN 978-0-618-91824-9" ],
+		'prog' => [ "200%", null ],
+		],
+		[
+		'book' => [ "EN", "Delusions of Gender", "Cordelia Fine" ],
+		'genr' => [ "Geschlechterforschung", "Gender Studies" ],
+		'desc' => [ "ISBN 978-184831-220-3" ],
+		'prog' => [ "100%", null ],
+		],
+		[
+		'book' => [ "DE", "Artgerecht ist nur die Freiheit", "Hilal Sezgin" ],
+		'genr' => [ "Tierrechte", "Animal Rights" ],
+		],
+		[
+		'book' => [ "DE", "Grenzen des Wachstums - das 30-Jahre-Update", "Donella Meadows, Jørgen Randers, Dennis Meadows" ],
+		'genr' => [ "Ökonomie", "Economy" ],
+		'desc' => [
+			"ISBN 978-3-7776-2228-6",
+			"",
+			"<br/>English:<br/>Limits to Growth: The 30-Year Update<br/>ISBN 978-1-9314-9858-6<br/>" . externStr("https://www.amazon.com/s/?field-keywords=ISBN+978-1-9314-9858-6", "(amazon.com)")
+		],
+		'prog' => [ "200%", null ],
+		],
+		[
+		'book' => [ "DE", "Das Kapital - Band 1", "Karl Marx" ],
+		'genr' => [ "Ökonomie", "Economy" ],
+		],
+		[
+		'book' => [ "DE", "Manifest der Kommunistischen Partei", "Karl Marx, Friedrich Engels" ],
+		'genr' => [ "Politik", "Politics" ],
+		],
+		[
+		'book' => [ "DE", "Arbeitsfrei", "Constanze Kurz, Frank Rieger" ],
+		'genr' => [ "Automatisierung", "Automation" ],
+		],
+		[
+		'book' => [ "DE", "Die Welt als Wille und Vorstellung", "Arthur Schopenhauer" ],
+		'genr' => [ "Philosophie", "Philosophy" ],
+		],
+		[
+		'book' => [ "DE", "Wie man mit Fundamentalisten diskutiert, ohne den Verstand zu verlieren", "Hubert Schleichert" ],
+		'genr' => [ "Philosophie", "Philosophy" ],
+		],
+		[
+		'book' => [ "DE", "Inside WikiLeaks", "Daniel Domscheit-Berg" ],
+		'genr' => [ "Journalismus", "Journalism" ],
+		],
+		[
+		'book' => [ "DE", "Ich war noch niemals auf Saturn", "Michael Büker" ],
+		'genr' => [ "Weltraum", "Space" ],
+		],
+		[
+		'book' => [ "EN", "What If? Serious Scientific Answers to Absurd Hypothetical Questions", "Randall Munroe" ],
+		'genr' => [ "Wissenschaft", "Science" ],
+		],
+		[
+		'book' => [ "EN", "Thing Explainer: Complicated Stuff in Simple Words", "Randall Munroe" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "DE", "Das Kochbuch für Geeks", "Mela Eckenfels, Petra Hildebrandt" ],
+		'genr' => [ "Kochbuch", "Cookbook" ],
+		],
+	];
+	### --- Science
+	### +++ Ender
+	$ender = [
+		[
+		'book' => [ "EN", "Ender's Game", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Speaker for the Dead", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Xenocide", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Children of the Mind", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Ender's Shadow", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Shadow of the Hegemon", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Shadow Puppets", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Shadow of the Giant", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Shadows in Flight", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Ender in Exile", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Earth Unaware", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Earth Afire", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Earth Awakens", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "The Swarm", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "First Meetings", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "A War of Gifts", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Children of the Fleet", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Orson Scott Card's Intergalactic Medicine Show", "Orson S. Card" ],
+		'genr' => $scifi,
+		],
+	];
+	### --- Ender
+	### +++ Voyager
+	$voyager = [
+		[
+		'book' => [ "DE", "Star Trek: Voyager 1 - Heimkehr", "Christie Golden" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "DE", "Star Trek: Voyager 2 - Ferne Ufer", "Christie Golden" ],
+		'genr' => $scifi,
+		],
+	];
+	### --- Voyager
+	### +++ Neal
+	$neal = [
+		[
+		'book' => [ "EN", "Cryptonomicon", "Neal Stephenson" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "The Diamond Age", "Neal Stephenson" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "Snow Crash", "Neal Stephenson" ],
+		'genr' => $scifi,
+		],
+	];
+	### --- Neal
+	### +++ Adams
+	$adams42 = [
+		[
+		'book' => [ "EN", "The Hitchhiker's Guide to the Galaxy", "Douglas Adams" ],
+		'genr' => $scifi,
+		],
+	];
+	### --- Adams
+	### +++ Heitz: Zwerge
+	$zwerge = [
+		[
+		'book' => [ "DE", function () use ($signed) { return "Die Zwerge" . $signed(); }, "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Krieg der Zwerge", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Rache der Zwerge", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Das Schicksal der Zwerge", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Triumph der Zwerge", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Sterne der Tiefen", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Botengang", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Zwerge: Band 1 - Tungdil", "Markus Heitz, Che Rossié, Yann Krehl" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "DE", "Die Zwerge: Band 2 - Der Thronanwärter", "Markus Heitz, Che Rossié, Yann Krehl" ],
+		'genr' => $comic,
+		],
+	];
+	### --- Heitz: Zwerge
+	### +++ Heitz: Albae
+	$albae = [
+		[
+		'book' => [ "DE", "Die Legenden der Albae", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Legenden der Albae: Vernichtender Hass", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Legenden der Albae: Dunkle Pfade", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Legenden der Albae: Tobender Sturm", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Die Legenden der Albae: Die Vergessenen Schriften", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+	];
+	### --- Heitz: Albae
+	### +++ Heitz: Uldart
+	$ulldart = [
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 1: Schatten über Uldart", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 2: Der Orden der Schwerter", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 3: Das Zeichen des Dunklen Gottes", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 4: Unter den Augen Tzulans", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 5: Die Magie des Herrschers", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Die Dunkle Zeit 6: Die Quellen des Bösen", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Zeit des Neuen 1: Trügerischer Friede", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Zeit des Neuen 2: Brennende Kontinente", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Zeit des Neuen 3: Fatales Vermächtnis", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Uldart - Zeit des Neuen 3: Fatales Vermächtnis", "Markus Heitz" ],
+		'genr' => $fantasy,
+		],
+	];
+	### --- Heitz: Uldart
+	### +++ Baron
+	$merlin = [
+		[
+		'book' => [ "DE", "Merlin - Wie alles begann", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlin und die Schritte zur Weisheit", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlin und die Feuerproben", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlin und der Zauberspiegel", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlin und die Flügel der Freiheit", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlins Drache 1 ", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlins Drache 2 - Die große Aufgabe", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Merlins Drache 3 - Die Schlacht der endlosen Feuer", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Zauber von Avalon 1 - Sieben Sterne und die dunkle Prophezeiung", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Zauber von Avalon 2 - Im Schatten der Lichtertore", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Der Zauber von Avalon 3 - Die ewige Flamme", "Thomas A. Barron" ],
+		'genr' => $fantasy,
+		],
+	];
+	### --- Baron
+	### +++ Tolkien
+	$tolkien = [
+		[
+		'book' => [ "DE", "Der kleine Hobbit", "John R. R. Tolkien" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Herr der Ringe 1: Die Gefährten", "John R. R. Tolkien" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Herr der Ringe 2: Die zwei Türme", "John R. R. Tolkien" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Herr der Ringe 3: Die Rückkehr des Königs", "John R. R. Tolkien" ],
+		'genr' => $fantasy,
+		],
+	];
+	### --- Tolkien
+	### +++ Potter
+	$potter = [
+		[
+		'book' => [ "DE", "Harry Potter und der Stein der Weisen", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und die Kammer des Schreckens", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und der Gefangene von Askaban", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und der Feuerkelch", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und der Orden des Phönix", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und der Halbblutprinz", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Harry Potter und die Heiligtümer des Todes", "Joanne K. Rowling" ],
+		'genr' => $fantasy,
+		],
+	];
+	### --- Potter
+	### +++ Millenium
+	$millenium = [
+		[
+		'book' => [ "DE", "Verblendung", "Stieg Larsson" ],
+		'genr' => $thriller,
+		],
+		[
+		'book' => [ "DE", "Verdammnis", "Stieg Larsson" ],
+		'genr' => $thriller,
+		],
+		[
+		'book' => [ "DE", "Vergebung", "Stieg Larsson" ],
+		'genr' => $thriller,
+		],
+		[
+		'book' => [ "DE", "Verschwörung", "David Lagercrantz (".$rcl->lang("nach", "after")." Stieg Larsson †)" ],
+		'genr' => $thriller,
+		],
+	];
+	### --- Millenium
+	### +++ Eschbach
+	$eschbach = [
+		[
+		'book' => [ "DE", "Black Out", "Andreas Eschbach" ],
+		'genr' => $thriller,
+		],
+		[
+		'book' => [ "DE", "Hide Out", "Andreas Eschbach" ],
+		'genr' => $thriller,
+		],
+		[
+		'book' => [ "DE", "Time Out", "Andreas Eschbach" ],
+		'genr' => $thriller,
+		],
+	];
+	### --- Eschbach
+	### +++ Novels
+	$novels = [
+		[
+		'book' => [ "EN", "Fight Club", "Chuck Palahniuk" ],
+		'genr' => $novel,
+		],
+		[
+		'book' => [ "DE", "Herr Lehmann", "Sven Regener" ],
+		'genr' => $novel,
+		],
+		[
+		'book' => [ "DE", "Sitzen vier Polen im Auto", "Alexandra Tobor" ],
+		'genr' => $novel,
+		],
+		[
+		'book' => [ "DE", "Minigolf Paradiso", "Alexandra Tobor" ],
+		'genr' => $novel,
+		],
+	];
+	### --- Novels
+	### +++ Love
+	$love = [
+		[
+		'book' => [ "EN", "Pride and Prejudice", "Jane Austen" ],
+		'genr' => $romance,
+		],
+		[
+		'book' => [ "EN", "Twilight", "Stephenie Meyer" ],
+		'genr' => $romance,
+		],
+		[
+		'book' => [ "EN", "New Moon", "Stephenie Meyer" ],
+		'genr' => $romance,
+		],
+		[
+		'book' => [ "EN", "Eclipse", "Stephenie Meyer" ],
+		'genr' => $romance,
+		],
+		[
+		'book' => [ "EN", "Breaking Dawn", "Stephenie Meyer" ],
+		'genr' => $romance,
+		],
+	];
+	### --- Love
+	### +++ Comics
+	$comics = [
+		[
+		'book' => [ "EN", "Questionable Content Vol. 1", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "Questionable Content Vol. 2", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "Questionable Content Vol. 3", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "Questionable Content Vol. 4", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "Questionable Content Vol. 5", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "Questionable Content Vol. 6", "Jeph Jacques" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "EN", "The GaMERCaT Volume 1", "Samantha Whitten" ],
+		'genr' => $comic,
+		],
+		[
+		'book' => [ "DE", function () use ($signed) { return "Massu Schmiedstochter" . $signed(); }, "Ines Korth" ],
+		'genr' => $comic,
+		],
+	];
+	### --- Comics
+	### +++ Artbooks/Guides
+	$artbooks = [
+		[
+		'book' => [ "DE", "Diablo 3 - Die Cain-Chronik", "Flint Dille" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Diablo 3 - Die Tyrael-Chronik", "Matt Burns, Doug Alexander" ],
+		'genr' => $fantasy,
+		],
+		[
+		'book' => [ "DE", "Star Wars - Handbuch der Imperialen Streitkräfte", "Daniel Wallace" ],
+		'genr' => $scifi,
+		],
+		[
+		'book' => [ "EN", "The Zombie Survival Guide", "Max Brooks" ],
+		'genr' => [ "Ratgeber", "Guide" ],
+		],
+	];
+	### --- Artbooks/Guides
+	
+	$books = array_merge(
+		$science,
+		// sci-fi
+		$ender,
+		$voyager,
+		$neal,
+		$adams42,
+		// fantasy
+		$zwerge,
+		$albae,
+		$ulldart,
+		$merlin,
+		$tolkien,
+		$potter,
+		// thriller
+		$millenium,
+		$eschbach,
+		// misc.
+		$novels,
+		$love,
+		$comics,
+		$artbooks,
+		[]
+	);
+	foreach ($books as $b) {
+		echo "\n<tr>";
+		
+		// Nr.
+		echo "  <td>".($nr++)."</td>";
+		
+		// language
+		echo "  <td>".$b['book'][0]."</td>";
+		
+		// title
+		$t = $b['book'][1];
+		echo "  <td>".(is_callable($t) ? $t() : $t);
+		if ($b['desc'] ?? 0) { isbn($b['desc'][0], $b['desc'][1], $b['desc'][2], $b['book'][0]); }
+		echo "</td>";
+		
+		// author
+		echo "  <td>".$b['book'][2]."</td>";
+		
+		// genre
+		$de = is_array($b['genr']) ? $b['genr'][0] ?? '' : $b['genr'] ?? '';
+		$en = is_array($b['genr']) ? $b['genr'][1] ?? '' : $b['genr'] ?? '';
+		echo "  <td>" . $rcl->lang($de, $en ?? $de) . "</td>";
+		
+		echo "  </tr>";
+	}
+?>
+</tbody>
+</table>
+
+<br/>
+
+<div>
+<?php if($rcl->lang === "de") { ?>
+Ja, ich lese viel.
+<?php } else { ?>
+Yes, I read a lot.
+<?php }?>
+</div>
 
 <?php include("include2.php"); ?>
