b6e6bf78cc344bd8ac0d3d75fde8f9559582fdc3
Robin C. Ladiges <rcl@blackpinguin.de>
2022-10-17 02:39:29 +0200

    WIP travanto

--- job/2015/index.php
+++ job/2015/index.php
@@ -2,6 +2,101 @@
 <?php $title = "Travanto Travel";  $path = "/job/2015"; ?>
 <?php include("../../include1.php"); ?>
 
+<pre>
+Languages:
+- Bash - often
+- CSS - often
+- Go - experimental
+- GraphQL - daily
+- HTML - daily
+- JavaScript - often
+- JSON - sometimes
+- PHP - daily in the past, now often
+- Scala - experimental
+- SCSS - often
+- SQL - often in the past, now sometimes (ORM)
+- TypeScript - daily
+- UML - rarely
+- XML - daily in the past, now rarely
+- XSLT - daily in the past, now rarely
+- YML - often
+
+Technologien:
+- Node.js
+- Apollo Server / Client
+- Sequelize
+- Vue
+- Apache
+- Docker
+- Portus
+- Traefik
+- Nginx
+- ajt
+- Webpack
+- Grunt
+- Composer
+- Behat
+- PHPUnit
+- phpDocumentor2
+- PHPTAL
+- JSON-Schema v4
+- Rest
+- GraphQL
+- Promises (PHP, JS/TS, Scala)
+- async.js
+- jQuery
+- (Angular)
+- cURL
+- MySQL
+- PostgreSQL
+- PostGIS
+- MongoDB
+- Solr
+- Git
+- Gitlab
+- Linux
+- Systemd
+- Munin
+- (Bacula)
+- AWS (Lambda, RDS, S3, EC2, ECS, SQS, SES, SNS, ALB/CLB, CloudFront, CloudWatch, API Gateway, IAM, awscli, PHP SDK, JS SDK)
+- Mapnik
+- Leaflet
+- Let's Encrypt
+- Puppetry
+- Puppeteer
+- (OwnCloud)
+- (Kybernetes)
+- ...
+
+Projects:
+- ...?
+- Travanto-Kleinkram (Weiterleitungen, Kundenaquise-Tool, Buchungssrecke-Redesign, CleverReach, Experteneinstellungen, ...)
+- Mietmeile (Weiterleitungen, Hierarchische Kategorien?)
+- Verwalter-API (Authorisierung, Rechtemanagement, Admin, JSON-Schemata, Preise, Bilder, Buchungen, Anfragen, Push, Integration Tests)
+- Property Management System Importe (Aufbau, Verwalter-API, V-Office, FEDER-Software, FewoPro, ICS)
+- Cloudumzug (Passwörter, Docker, Deployment, Gitlab)
+- Harz- und Sauerland-Travel (Git, Merge beider Projekte, PHP7, Umprogrammierung aller Datenbankzugriffe, Solr)
+- OpenStreetMap Tile Server
+- Reichweite+ (Kundenzuordnung, Landingpage, T2Q, OB Refak I, Verwalter-API, Airbnb, Availability Helper, Booking.com)
+- BookedByClick (Sync2Travanto, GraphQL, Admin, Cloudmail, Redesign, ICS)
+
+Fortbildungsgruppen:
+- [2016] Linux / Infrastruktur
+  - Bacula
+- [2017] Serverless mit AWS Lambda
+  - Node-Lambda
+  - JS Patterns
+- [2017-2018] Software Architektur
+- [2018] Web Development 2025
+  - GraphQL in Scala
+- [2019] Expert Programming
+- [2019] Test Automation und Testen vom Frontend
+  - Puppetry
+  - Puppeteer
+- [2019-2020] Hacking
+  - Wireshark
+</pre>
+
 <div>
 <?php if($rcl->lang === "de") { ?>
 TODO
